37e474b5d355883542814421bff69fa315912c1b
Hakata932 <carlos.emanuel932@gmail.com>
2024-06-05 13:06:22 -0500

    SAVE

--- trabalhos javascript/aula1.js
+++ trabalhos javascript/aula1.js
@@ -5,3 +5,8 @@ var dinheiro = 24.99
 document.write(nome + "<br>")
 document.write(idade)
 document.write(dinheiro)
+
+console.log(nome)
+alert(idade)
+var meuNome = prompt("Qual o seu nome?")
+document.write("Meu nome é " + meuNome)
---
+++ trabalhos javascript/aula2.js
@@ -0,0 +1,8 @@
+var nome = "Cadete"
+var num = 20
+var mensagem = "Bem-vindo."
+document.write(nome + "<br>")
+document.write(num)
+var seuNome = prompt("Digite seu nome")
+var idade = prompt("Digite sua idade")
+alert("Seu nome é " + seuNome + ", " + idade + " anos, " + mensagem)
