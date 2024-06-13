64b802ce33e5602e061f3e4414dd48b347368f73
Hakata932 <carlos.emanuel932@gmail.com>
2024-06-13 14:19:07 -0500

    dia 13/06

---
+++ trabalhos javascript/atv1.js
@@ -0,0 +1,6 @@
+var num = Number(prompt("Verificação de número igual a zero, digite um número: "))
+if(num == 0){
+    document.write("O número é igual a 0.")
+} else{
+    document.write("Nada")
+}
--- trabalhos javascript/aula3.js
+++ trabalhos javascript/aula3.js
@@ -1,8 +1,12 @@
-var pessoa1 = 1
-var pessoa2 = 1
-var soma = pessoa1 + pessoa2
-if(soma >= 2){
+var pessoa1 = Number(prompt("Quantas pessoas estarão na viagem?"))
+var pessoa2 = Number(prompt("Quantas crianças estarão na viagem?"))
+var soma = pessoa1 + pessoa2 
+if(soma == 2 && pessoa2 == 0){
     alert("Detectado duas pessoas na viagem, liberado desconto de 10%")
-} else{
+}
+else if(soma == 3 || pessoa2 == 1){
+    alert("Detectado duas pessoas e uma criança na viagem, liberado desconto de 15%")
+}
+else{
     alert("pagamento normal.")
 }
