711d13238f6c6bbe7b890ed2ec59ec4e1a79e06b
Hakata932 <carlos.emanuel932@gmail.com>
2024-06-20 14:30:44 -0500

    dia 20/06

---
+++ trabalhos javascript/atv6.js
@@ -0,0 +1,60 @@
+//while(true){
+//    var nome = prompt("Digite seu nome")
+//
+//  if(nome === "Carlos"){
+//        alert("Olá, " + nome)
+//        break
+//    } else{
+//        alert("Não é o seu nome, digite novamente")
+//    }
+//}
+
+//var nome = prompt("Escreva seu nome")
+//var qnt = 1
+//for(qnt; qnt <= 5; qnt++){
+//    document.write(nome + "<br>")
+//}
+
+//var num = 0
+//while(num <= 70){
+//    document.write(num + "<br>")
+//    num = num + 2
+//}
+
+//while(true){
+//    var numUsuario = Number(prompt("Digite um número de 0 a 5 para se adivinhar corretamente"))
+//    var numAleatorio = Math.floor(Math.random() * 5)
+//        alert("Você digitou: " + numUsuario + ", a máquina digitou: " + numAleatorio)
+//        if(numAleatorio == numUsuario){
+//        alert("Você acertou o número.")
+//        document.write("Reinicie a página para resetar o jogo.")
+//        break
+//    } else{
+//        alert("Tente novamente")
+//    }
+//}
+
+//var contador = 1
+//while(contador != 50){
+//    contador = contador + 1
+//    if(contador == 30){
+//        document.write(contador)
+//        break
+//    } 
+//}
+
+//var num = 0
+//while(num < 20){
+//    num = num + 1
+//    if(num % 2 == 0){
+//        document.write(num + "<br>")
+//    }
+//}
+
+var num = 0
+while(num < 700){
+    num++
+    if(num <= 90){
+        document.write(num + "<br>")
+    }
+}
