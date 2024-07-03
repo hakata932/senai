651fa3d126019c5af8a8ed2a3a7abfb5430af62c
Hakata932 <carlos.emanuel932@gmail.com>
2024-07-03 14:47:03 -0500

    .

---
+++ trabalhos javascript/atv9.js
@@ -0,0 +1,7 @@
+//var estados = ['Acre', 'Rondônia', 'Amazonas', 'Amapá', 'Pará']
+//document.write(estados[4])
+
+var estados = ['Acre', 'Rondônia', 'Amazonas', 'Amapá', 'Pará']
+estados.push('Tocantins')
+estados.splice(2, 5)
+document.write(estados)
