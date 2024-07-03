1f9a4c536463f1ed553960446a795cbe598a701b
Hakata932 <carlos.emanuel932@gmail.com>
2024-07-03 14:27:45 -0500

    dia 03/07

--- trabalhos javascript/aula5.js
+++ trabalhos javascript/aula5.js
@@ -1,2 +1,24 @@
-var estados = ["Acre", "Rio de Janeiro", "São Paulo"]
-document.write(estados[1])
+//var estados = ['Rondônia', 'Acre', 'Rio de Janeiro', 'São Paulo']
+//var encontrado = estados.includes("Acre")
+//document.write(`O estado do Acre está no array? ${encontrado}`)
+
+//var lista = [
+//    'GIGN',
+//    'bope',
+//    'tropa de elite',
+//    'JJDJD',
+//]
+//document.write(lista.indexOf('bope'))
+
+//var estados = ['Rondônia', 'Acre', 'Rio de Janeiro', 'São Paulo']
+//var qual_estado = 'São Paulo'
+//var des_indice = estados.indexOf(qual_estado)
+//if(des_indice !== -1){
+//    alert(`O nome ${qual_estado} foi encontrado no indice ${des_indice}`)
+//} else{
+//    alert(`O nome ${qual_estado} não foi encontrado em nenhuma posição.`)
+//}
+
+var carros = ['BMW', 'Ferrari', 'Gol', 'Civic']
+carros[2] = 'corolla'
+alert(carros)
