e2421dde180ac9df265e5ac9533664f9cdd9f6eb
Hakata932 <carlos.emanuel932@gmail.com>
2024-08-07 14:54:27 -0500

    dia 07/08

---
+++ js DOM/dom.js
@@ -0,0 +1,9 @@
+var meuh1 = document.querySelector('#meuh1');
+
+meuh1.innerHTML = 'Alterei o texto no JS'
+
+meuh1.style.color = 'red'
+
+var meup = document.querySelector('#meup')
+meup.innerHTML = 'Este texto foi alterado'
+meup.style.color = 'blue'
