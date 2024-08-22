2e8b1511046a642847c9953fbe9998e4e2e17980
Hakata932 <carlos.emanuel932@gmail.com>
2024-08-22 13:47:04 -0500

    primeira aula de SQL
    lista de comandos mais importantes e usados da aula de hoje

---
+++ SQL/comandos.sql
@@ -0,0 +1,32 @@
+/* comandos para criar database (banco de dados*/
+CREATE DATABASE senai;
+
+/*comandos para criar tabela */
+
+CREATE TABLE aluno (
+    id int AUTO_INCREMENT primary key,
+    nome varchar(255) not null,
+    email varchar(255)
+);
+
+/*comando para alterar um caractere dentro da tabela*/
+
+ALTER TABLE aluno
+CHANGE nome nome_pessoal varchar(255);
+
+/*renomear nome da tabela*/
+
+RENAME TABLE aluno TO alunos;
+
+/*Comando para apagar um campo da tabela tabela*/
+
+ALTER TABLE alunos DROP COLUMN nome;
+
+/*adicionando campo após tabela criada*/
+
+ALTER TABLE alunos
+ADD descricao varchar(255);
+
+/*inserindo valores dentro de uma tabela*/
+
+INSERT INTO alunos (nome_pessoal, email) VALUES ("Joãozinho do Mato Souza", "joão@gmail.com");
