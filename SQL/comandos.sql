3c2fccc46b2fec82b9e9aae26a848904127f6b66
Hakata932 <carlos.emanuel932@gmail.com>
2024-08-23 13:48:39 -0500

    operadores lógicos no SQL

--- SQL/comandos.sql
+++ SQL/comandos.sql
@@ -6,7 +6,8 @@ CREATE DATABASE senai;
 CREATE TABLE aluno (
     id int AUTO_INCREMENT primary key,
     nome varchar(255) not null,
-    email varchar(255)
+    email varchar(255),
+    idade int
 );
 
 /*comando para alterar um caractere dentro da tabela*/
@@ -36,3 +37,19 @@ INSERT INTO alunos (nome_pessoal, email) VALUES ("Joãozinho do Mato Souza", "jo
 UPDATE alunos
 SET email = 'novoemail@gmail.com', nome_pessoal = 'novo nome'
 WHERE id = 1;
+
+/*Atualizando valores ou inserindo valores não preenchidos previamente enquanto se insere uma condição*/
+
+UPDATE alunos
+SET nome_pessoal = 'novo nome'
+WHERE idade > 30;
+
+/*Deletando dados de uma tabela exemplo:*/
+
+DELETE FROM clientes WHERE id = 1
+
+/*operador lógico em SQL*/
+
+--SELECT estudantes FROM alunos WHERE estudantes = 'Cazaquistão' AND idade_dos_estudantes = 17;
+
+--SELECT estudantes FROM alunos WHERE estudantes = 'Cazaquistão' OR idade_dos_estudantes = 16;
